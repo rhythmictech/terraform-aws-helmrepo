bbffcdb746c2f792eb071765e13d25256e8e4d23
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-07-02 17:21:21 -0400

    added crossaccount support

--- main.tf
+++ main.tf
@@ -4,10 +4,31 @@ data "aws_caller_identity" "current" {
 data "aws_region" "region" {
 }
 
+locals {
+  bucket_name = var.name != null ? var.name : "${data.aws_caller_identity.current.account_id}-${data.aws_region.region.name}-${var.name_suffix}"
+
+  logging_map = var.logging_bucket == null ? [] : [{
+    bucket = var.logging_bucket
+    prefix = var.logging_bucket_prefix != null ? var.logging_bucket_prefix : local.bucket_name
+  }]
+}
+
+# This bucket uses a dynamic block to generate logging. If users do not wish to log,
+# that's on them, but the module supports it.
+#tfsec:ignore:AWS002
 resource "aws_s3_bucket" "this" {
-  bucket = "${data.aws_caller_identity.current.account_id}-${data.aws_region.region.name}-${var.name}-helm-repo"
+  bucket = local.bucket_name
   acl    = "private"
   tags   = var.tags
+
+  dynamic "logging" {
+    for_each = local.logging_map
+
+    content {
+      target_bucket = logging.value.bucket
+      target_prefix = logging.value.prefix
+    }
+  }
 
   server_side_encryption_configuration {
     rule {
@@ -20,7 +41,6 @@ resource "aws_s3_bucket" "this" {
   versioning {
     enabled = true
   }
-
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
@@ -35,4 +55,80 @@ resource "aws_s3_bucket_public_access_block" "this" {
   }
 
   depends_on = [aws_s3_bucket.this]
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid       = "DenyIncorrectEncryptionHeader"
+    actions   = ["s3:PutObject*"]
+    effect    = "Deny"
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["AES256"]
+    }
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
+
+  statement {
+    sid       = "DenyUnEncryptedObjectUploads"
+    actions   = ["s3:PutObject*"]
+    effect    = "Deny"
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = [true]
+    }
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.allowed_account_ids
+
+    content {
+
+      sid       = "Allow cross-account access to list objects (${statement.value})"
+      actions   = ["s3:ListBucket"]
+      effect    = "Allow"
+      resources = [aws_s3_bucket.this.arn]
+
+      principals {
+        identifiers = ["arn:aws:iam::${statement.value}:root"]
+        type        = "AWS"
+      }
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.allowed_account_ids
+
+    content {
+      sid       = "Allow Cross-account read-only access (${statement.value})"
+      actions   = ["s3:GetObject*"]
+      effect    = "Allow"
+      resources = ["${aws_s3_bucket.this.arn}/*"]
+
+      principals {
+        identifiers = ["arn:aws:iam::${statement.value}:root"]
+        type        = "AWS"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,33 @@
+########################################
+# General Vars
+########################################
+variable "allowed_account_ids" {
+  default     = []
+  description = "List of AWS account IDs to grant read-only access to the repo. Due to how policies are constructed, there's effectively a limit of about 9 accounts."
+  type        = list(string)
+}
+
+variable "logging_bucket" {
+  default     = null
+  description = "S3 bucket name to log bucket access requests to (optional)"
+  type        = string
+}
+
+variable "logging_bucket_prefix" {
+  default     = null
+  description = "S3 bucket prefix to log bucket access requests to (optional). If blank but a `logging_bucket` is specified, this will be set to the name of the bucket"
+  type        = string
+}
+
 variable "name" {
-  default     = "default"
-  description = "Prefix name for the helm repo (e.g., EKS cluster name)"
+  default     = null
+  description = "Bucket name for the helm repo. Specify to control the exact name of the bucket, otherwise use `name_suffix`"
+  type        = string
+}
+
+variable "name_suffix" {
+  default     = "helmrepo"
+  description = "Bucket suffix for the repo (bucket will be named `[ACCOUNT_ID]-[REGION]-[name_suffix]`, not used if `name` is specified)"
   type        = string
 }
 
